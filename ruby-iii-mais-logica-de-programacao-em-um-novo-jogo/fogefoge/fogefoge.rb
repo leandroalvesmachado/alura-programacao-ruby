d7afada2205e1ba81a58f2749469ebd48bb3b7c5
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-05-19 21:30:55 -0300

    salvando

--- ruby-iii-mais-logica-de-programacao-em-um-novo-jogo/fogefoge/fogefoge.rb
+++ ruby-iii-mais-logica-de-programacao-em-um-novo-jogo/fogefoge/fogefoge.rb
@@ -30,17 +30,33 @@ end
 
 
 def calcula_nova_posicao(heroi, direcao)
-    case direcao
-        when "W"
-            heroi[0] -= 1
-        when "S"
-            heroi[0] += 1
-        when "A"
-            heroi[1] -= 1
-        when "D"
-            heroi[1] += 1
-    end
+    heroi = heroi.dup
+    
+    # case direcao
+    #     when "W"
+    #         heroi[0] -= 1
+    #     when "S"
+    #         heroi[0] += 1
+    #     when "A"
+    #         heroi[1] -= 1
+    #     when "D"
+    #         heroi[1] += 1
+    # end
 
+    # otimizando o codigo
+    # usando chaves ({) para array associativo
+    movimentos = {
+        "W" => [-1, 0],
+        "S" => [+1, 0],
+        "A" => [0, -1],
+        "D" => [0, +1]
+    }
+
+    movimento = movimentos[direcao]
+    heroi[0] += movimento[0]
+    heroi[1] += movimento[1]
+
+    # return heroi
     heroi
 end
 
@@ -49,6 +65,7 @@ end
 def posicao_valida?(mapa, posicao)
     linhas = mapa.size
     colunas = mapa[0].size
+    # comparacao que retorna true ou false
     estourou_linhas = posicao[0] < 0 || posicao[0] >= linhas
     estourou_colunas = posicao[1] < 0 || posicao[1] >= colunas
 
@@ -56,7 +73,8 @@ def posicao_valida?(mapa, posicao)
         return false
     end
 
-    if mapa[posicao[0]][posicao[1]] == "X"
+    valor_atual = mapa[posicao[0]][posicao[1]]
+    if valor_atual == "X" || valor_atual == "F"
         return false
     end
 
@@ -65,9 +83,48 @@ def posicao_valida?(mapa, posicao)
 end
 
 
+def posicoes_validas_a_partir_de(mapa, posicao)
+    posicoes = []
+    baixo = [posicao[0] + 1, posicao[1]]
+    direita = [posicao[0], posicao[1] + 1]
+    cima = [posicao[0] - 1, posicao[1]]
+    esquerda = [posicao[0], posicao[1] - 1]
+
+    posicoes
+end
+
+
+def move_fantasma(mapa, linha, coluna)
+    puts "Movendo o fantasma da posição #{linha} #{coluna}"
+    posicoes = posicoes_validas_a_partir_de mapa, [linha, coluna]
+
+    # outra forma return if posicoes.empty?
+    if posicoes.empty?
+        return
+    end
+
+    posicao = posicoes[0]
+    mapa[linha][coluna] = " "
+    mapa[posicao[0]][posicao[1]] = "F"
+end
+
+def move_fantasmas(mapa)
+    caractere_do_fantasma = "F"
+
+    mapa.each_with_index do |linha_atual, linha|
+        # linha_atual.chars = transforma uma string em um array de caracteres
+        linha_atual.chars.each_with_index do |caractere_atual, coluna|
+            he_fantasma = caractere_atual == caractere_do_fantasma
+            if he_fantasma
+                move_fantasma mapa, linha, coluna
+            end
+        end
+    end
+end
+
 def joga(nome)
     # equivalente a mapa = le_mapa(1)
-    mapa = le_mapa 1
+    mapa = le_mapa 2
 
     while true
         # equivalente a desenha(mapa)
@@ -82,6 +139,7 @@ def joga(nome)
 
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+        move_fantasmas mapa
     end
 end
 
